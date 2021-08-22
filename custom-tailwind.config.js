670e0c3d3df56d67709b32fc79e6137e4434dafc
harry Nguyen <hpnguyen52@gmail.com>
2021-08-22 17:04:17 -0400

    updated readme and minor refactor

--- custom-tailwind.config.js
+++ custom-tailwind.config.js
@@ -137,6 +137,17 @@ const fontSize = {
     'normalize': '62.5%',
 }
 
+const boxShadow = {
+    sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+    DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+    md: '0px 3px 5px -1px rgb(0 0 0 / 10%), 0px 6px 10px 0px rgb(0 0 0 / 6%), 0px 1px 18px 0px rgb(0 0 0 / 4%)',
+    lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+    xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+    '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+    inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+    none: 'none',
+}
+
 module.exports = {
     prefix: '',
     purge: {
@@ -167,22 +178,13 @@ module.exports = {
         colors: colors,
         borderRadius: {
             'none': '0px',
-            'sm': '2px',
-            DEFAULT: '4px',
-            'lg': '8px',
+            'sm': '4px',
+            DEFAULT: '6px',
+            'lg': '12px',
             'xl': '16px',
             full: '9999px',
         },
-        boxShadow: {
-            sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
-            DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
-            md: '0px 3px 5px -1px rgb(0 0 0 / 10%), 0px 6px 10px 0px rgb(0 0 0 / 6%), 0px 1px 18px 0px rgb(0 0 0 / 4%)',
-            lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-            xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
-            '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
-            inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
-            none: 'none',
-        },
+        boxShadow: boxShadow,
         fontFamily: {
             display: ['Rawline', 'sans-serif'],
             body: ['Rawline', 'sans-serif'],
